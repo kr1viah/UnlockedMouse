8f61d5c62c003e1cef107adeb87ff43ea0dd6622
kr1viah <kravpolet@gmail.com>
2026-09-01 17:41:56 +0200

    FIX IT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "2.3.0"
     id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT"
